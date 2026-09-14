77a3a8d8d6700674d53d31b65cae02fac00a70b2
Fermain :) <fermainforeromadrigal@gmail.com>
2026-09-14 01:41:12 +0000

    git commit -m

--- Taller_6/taller6.py
+++ Taller_6/taller6.py
@@ -1,8 +1,11 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier, export_text
 
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier, export_text
+
 # 1. Dataset de Entrenamiento (Marketing - 10 Clientes)
-# Columnas X: [Edad, Horas_Online, Compras_Previas][cite: 6, 7]
+# Columnas X: [Edad, Horas_Online, Compras_Previas]
 X = np.array([
     [18, 5, 0],
     [22, 8, 1],
@@ -16,16 +19,16 @@ X = np.array([
     [25, 6, 2]
 ])
 
-# Etiqueta Y: [1: Hizo clic, 0: Lo ignoró][cite: 6, 7]
+# Etiqueta Y: [1: Hizo clic, 0: Lo ignoró]
 Y = np.array([0, 1, 0, 0, 0, 1, 0, 1, 0, 1])
 
 # 2. Entrenamiento del Modelo
-arbol = DecisionTreeClassifier(max_depth=3)[cite: 6, 7]
-arbol.fit(X, Y)[cite: 6, 7]
+arbol = DecisionTreeClassifier(max_depth=3)
+arbol.fit(X, Y)
 
 # 3. Extracción e Impresión de Reglas
 nombres_variables = ["Edad", "Horas_Online", "Compras_Previas"]
-reglas_texto = export_text(arbol, feature_names=nombres_variables)[cite: 6, 7]
+reglas_texto = export_text(arbol, feature_names=nombres_variables)
 
 print("--- BASE DE REGLAS GENERADA AUTOMÁTICAMENTE ---")
 print(reglas_texto)
